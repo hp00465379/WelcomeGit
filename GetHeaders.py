41f4d10c75cec8c360c6cebb6b25b0dc21f328e9
hp00465379 <wookjun@gmail.com>
2023-04-27 15:24:37 +0900

    Updated GetHeaders.py

--- GetHeaders.py
+++ GetHeaders.py
@@ -1,6 +1,6 @@
 import requests
 
-headerdata = {'T1':'First_Header','T2':'Second_Header'}
+headerdata = {'T1':'First_Header','T2':'Second_Header_Header'}
 
 response = requests.get('https://httpbin.org/get',headers=headerdata,verify=False)
 print(response.text)
